b1f61a5e08b5ad6506044aae60bbe9eb81dd4eb1
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-10-31 11:26:48 -0700

    Build Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,21 @@
+import "../sass/Form.scss";
+
 const Form = () => {
-  return <form>Form</form>;
+  return (
+    <form className="form">
+      <input type="text" placeholder="First Name" />
+      <input type="text" placeholder="Last Name" />
+      <input type="email" placeholder="Email Address" />
+      <input type="password" placeholder="Password" />
+      <button type="submit" className="submitBtn">
+        Claim your free trial
+      </button>
+      <p>
+        By clicking the button, you are agreeing to our{" "}
+        <span>Terms and Services</span>
+      </p>
+    </form>
+  );
 };
 
 export default Form;
